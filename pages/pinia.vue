fb037bb9dd913586daa9de7c6a0e037a656cb996
ppppppsmash <pppppp.work@gmail.com>
2023-07-08 23:42:03 +0900

    add: piniaインストール&テストファイル用意

---
+++ pages/pinia.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useCounterStore } from "@/store/counter";
+import { storeToRefs } from "pinia";
+const counterStore = useCounterStore();
+const { increment } = counterStore;
+const { count } = storeToRefs(counterStore);
+</script>
+
+<template>
+  <div>
+    <h1>Count: {{ count }}</h1>
+    <button @click="increment">プラス</button>
+    <br />
+    <!-- <nuxt-link to="/">もどる</nuxt-link> -->
+  </div>
+</template>
