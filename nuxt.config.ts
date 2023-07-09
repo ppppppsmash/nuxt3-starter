7b78053be6b47af3ead9431de53d3f7fd726fb13
ppppppsmash <pppppp.work@gmail.com>
2023-07-09 10:38:10 +0900

    add: meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,36 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@pinia/nuxt'],
+  app: {
+    head: {
+      title: 'nuxt3 starter',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: '' },
+        { hid: 'description', name: 'description', content: 'nuxt3 starter meta desc' },
+        { name: 'format-detection', content: 'telephone-no' }
+      ],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family-Roboto&display=swap'
+        },
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.ico'
+        },
+        // {
+        //   rel: 'stylesheet',
+        //   href: ''
+        // }
+      ],
+      // script: [
+      //   {
+      //     src: '',
+      //     type: 'text/javascript'
+      //   }
+      // ]
+    }
+  }
 })
