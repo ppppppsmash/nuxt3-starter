7af0e801181e40b0922a6c035664978f50d2262b
ppppppsmash <pppppp.work@gmail.com>
2023-07-10 23:38:46 +0900

    add: Headerコンポーネント、NuxtLinkなど

---
+++ components/TheHeader.vue
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+<style>
+
+</style>
+
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="/">
+            TOP
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about">
+            ABOUT
+          </NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
