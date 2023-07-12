90f0bbac3fc78b84092e27636a9c3cdaab9902d6
ppppppsmash <pppppp.work@gmail.com>
2023-07-12 22:22:41 +0900

    mod: contactコンポーネント

--- pages/toy/[name]-[id].vue
+++ pages/toy/[name]-[id].vue
@@ -14,21 +14,7 @@
 
     <ToyDetailDescrisption />
 
-    <!-- TOY CONTACT -->
-    <div class="mt-10">
-      <div class="flex w-[600px] justify-between">
-        <input type="text" class="border p-1" placeholder="Name" />
-        <input type="text" class="border p-1" placeholder="Email" />
-        <input type="text" class="border p-1" placeholder="Phone" />
-      </div>
-      <div class="flex mt-4 w-[600px]">
-        <textarea class="border p-1 w-full" placeholder="Message"></textarea>
-      </div>
-      <button class="bg-gray-950 text-white px-10 py-3 rounded mt-4">
-        送信
-      </button>
-    </div>
-    <!-- TOY CONTACT -->
+    <ToyDetailContact />
   </div>
   <!-- TOY DETAIL PAGE   -->
 
