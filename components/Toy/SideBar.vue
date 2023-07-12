e0aa780516ff82bd591fc3cc2c74598ffd211384
ppppppsmash <pppppp.work@gmail.com>
2023-07-12 21:36:15 +0900

    add: sidebar

---
+++ components/Toy/SideBar.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+  <div
+    class="shadow border w-[20rem] mr-10 z-30 h-[190px]"
+  >
+    <div class="p-5 flex justify-between relative cursor-pointer border-b">
+      <h3>テキスト</h3>
+      <h3 class="text-blue-400 capitalize">テキスト2</h3>
+    </div>
+
+    <div class="p-5 flex justify-between relative cursor-pointer border-b">
+      <h3>ブランド</h3>
+      <h3 class="text-blue-400 capitalize">Takara Tomy</h3>
+    </div>
+
+    <div class="p-5 flex justify-between relative cursor-pointer">
+      <h3>値段</h3>
+      <h3 class="text-blue-400 capitalize">123</h3>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
--- pages/brand/[brand]/toy/[[make]].vue
+++ pages/brand/[brand]/toy/[[make]].vue
@@ -3,7 +3,38 @@
 </script>
 
 <template>
-  <div>
-    brand & make
+  <div
+    class="mx-auto mt-4 max-w-7xl space-y-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
+  >
+    <div class="mt-32 flex">
+
+      <ToySideBar />
+
+      <div class="w-full">
+        <div
+          class="shadow border w-full overflow-hidden mb-5 cursor-pointer b-[200px]"
+        >
+          <div class="flex h-full">
+            <img
+              src="https://carwow-uk-wp-3.imgix.net/Volvo-XC40-white-scaled.jpg"
+              alt=""
+              class="w-[300px] h-full"
+            />
+            <div class="p-4 flex flex-col">
+              <div>
+                <h1 class="text-2xl text-blue-700">コンボイ</h1>
+                <p class="text-gray-700">
+                  テキスト
+                </p>
+              </div>
+              <h1 class="mt-auto text-xl">￥10</h1>
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
   </div>
 </template>
