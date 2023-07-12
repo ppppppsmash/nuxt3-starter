3edfdb04c4d8de8e3ac7dc567164c9c6f88670cf
ppppppsmash <pppppp.work@gmail.com>
2023-07-12 22:14:58 +0900

    add: detailコンポーネント

---
+++ components/Toy/Detail/Hero.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+
+    <div class="mt-10">
+      <img
+        src="https://static.mercdn.net/item/detail/orig/photos/m26586674075_1.jpg"
+        class="w-full"
+        alt=""
+      />
+      <h1 class="mt-10 text-4xl">コンボイ</h1>
+      <div
+        class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between"
+      >
+        <div class="flex">
+          <p class="mr-2">ブランド: Takara Tomy</p>
+          <p class="mr-2">|</p>
+          <p class="mr-2">発売日: 2000年</p>
+        </div>
+        <div>
+          <p class="font-bold text-2xl">￥24,200円</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style>
+
+</style>
--- pages/toy/[name]-[id].vue
+++ pages/toy/[name]-[id].vue
@@ -7,30 +7,9 @@
   <div
     class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
   >
-    <div>
-      <!-- TOY HERO -->
-      <div class="mt-10">
-        <img
-          src="https://static.mercdn.net/item/detail/orig/photos/m26586674075_1.jpg"
-          class="w-full"
-          alt=""
-        />
-        <h1 class="mt-10 text-4xl">コンボイ</h1>
-        <div
-          class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between"
-        >
-          <div class="flex">
-            <p class="mr-2">ブランド: Takara Tomy</p>
-            <p class="mr-2">|</p>
-            <p class="mr-2">発売日: 2000年</p>
-          </div>
-          <div>
-            <p class="font-bold text-2xl">￥24,200円</p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- TOY HERO -->
+
+    <ToyDetailHero />
+
     <!-- TOY ATTRIBUTES -->
     <div class="mr-10 mt-5 border-b pb-5">
       <div class="flex text-lg mt-2">
