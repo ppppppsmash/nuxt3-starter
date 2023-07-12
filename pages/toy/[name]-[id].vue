6cf34f8298c87c83ebf93fa35b3aa2db7e49e3e5
ppppppsmash <pppppp.work@gmail.com>
2023-07-12 22:20:54 +0900

    mod: detailコンポーネント

--- pages/toy/[name]-[id].vue
+++ pages/toy/[name]-[id].vue
@@ -12,16 +12,8 @@
 
     <ToyDetailAttributes />
 
-    <!-- TOY DESCRISPTION -->
-    <div class="mt-5">
-      <p class="mb-10">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem
-        consequuntur, et saepe sequi, ipsum quisquam iste autem quas vel aperiam
-        laboriosam dolorum vero tempore placeat, asperiores alias magni. Nulla,
-        dolores.
-      </p>
-    </div>
-    <!-- TOY DESCRISPTION -->
+    <ToyDetailDescrisption />
+
     <!-- TOY CONTACT -->
     <div class="mt-10">
       <div class="flex w-[600px] justify-between">
