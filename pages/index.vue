12a14bc0e1977e9bc966c458feab44e7d0e022aa
ppppppsmash <pppppp.work@gmail.com>
2023-07-19 00:58:13 +0900

    add: apiで映画を検索

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,40 @@
-<script setup>
-  useHead({
-    title: 'トップページ',
-    meta: [
-      { name: 'description', content: 'nuxt3.0ページ' }
-    ],
-    bodyAttrs: {
-      class: 'test'
-    },
-    script: [ { innerHTML: 'console.log(\'Hello world\')' } ]
-  })
-  import { APIResponse } from '@/types/APIResponse';
+<script setup lang="ts">
+  import { APIResponse } from "@/types/APIResponse"
 
-  const searchTerm = ref('');
+  const searchTerm = ref("")
 
   const url = computed(() => {
     return `api/movies/search?query=${searchTerm.value}`
-  });
+  })
 
-  const { data } = await useFetch<APIResponse>(url);
+  const { data } = await useFetch<APIResponse>(url)
 </script>
+
+<template>
+  <div class="flex flex-col py-10">
+    <div>
+      <h2 class="text-2xl font-bold text-center">
+        ARATA's 映画APP
+      </h2>
+      <div class="flex justify-center items-center h-32">
+        <input
+          class="px-2 py-1 border border-gray-800 rounded-md min-w-64"
+          v-model="searchTerm"
+          type="text"
+          placeholder="映画を検索..."
+        />
+      </div>
+
+      <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4
+      xl:grid-cols-5 self-center gap-x-10 gap-y-10 mb-10">
+        <div v-for="movie in data?.results">
+          {{ movie.title }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <style lang='scss'>
 
 </style>
-
-<template>
-  <div>Hello world</div>
-</template>
--- server/api/movies/search.ts
+++ server/api/movies/search.ts
@@ -1,12 +1,16 @@
-export default defineEventHandler((event) => {
+export default async (event: any) => {
   const { query } = getQuery(event)
   const config = useRuntimeConfig()
-  return $fetch(
-    `${config.apiBaseUrl}/search/movie?query=${query}`,
+
+  const result = await $fetch(
+    `${config.apiBaseUrl}/search/movie?query=${query}&api_key=${config.apiKey}`,
     {
       method: "GET",
       headers: {
-        "Authorization": `Bearer ${config.apiKey}`
+        Authorization: `Bearer ${config.apiKey}`
       }
-    })
-})
+    }
+  )
+
+  return result
+}
